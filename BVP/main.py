4cdc2aaf967bf5cbbbd01c06bd09276dbef4dcb6
Shailesh <shailesh.k.mishra351@gmail.com>
2021-09-09 16:36:46 +0530

    updating main

--- BVP/main.py
+++ BVP/main.py
@@ -10,27 +10,13 @@ app = Flask(__name__)
 #CORS(app)
 
 
+app.register_blueprint(user_api)
+
+
 if __name__ == "__main__":
     # registering user API's
-    app.register_blueprint(user_api)
     # Registering questions API's
     app.register_blueprint(question_api)
     app.register_blueprint(report_api)
-    #propList = read_properties_file('bvp_config.properties')
-    # if len(propList['host']) > 0:
-    #     if len(os.environ.get("PORT", propList['port'])) > 0:
-    #         print("Running app with both host and port")
-    #         app.run(host=propList['host'], port=int(os.environ.get("PORT", propList['port'])), threaded=True)
-    #     else:
-    #         print("Running app with host")
-    #         app.run(host=propList['host'], threaded=True)
-    # elif len(os.environ.get("PORT", propList['port'])) > 0:
-    #     print("Running app with port")
-    #     app.run(port=int(os.environ.get("PORT", propList['port'])), threaded=True)
-    # else:
-    #     print("Running app with default settings")
-    #     app.run(threaded=True)
-
-    #port = int(os.environ.get("PORT", 5000))
     print("searching for URL")
     app.run()
